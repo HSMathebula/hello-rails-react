2881b6e4a7f45af22773db53dcc0d496cbf15be8
HSMathebula <56921581+HSMathebula@users.noreply.github.com>
2022-10-07 15:51:27 +0200

    create a configureStore.js file to configure store

--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,14 @@
 Rails.application.routes.draw do
-  root 'root#index'
   namespace :api do
     namespace :v1, defaults: {format: 'json'} do
       resources :greetings, only: [:index]
     end
   end
+
+  # get '*page', to: 'root#index', constraints: ->(req) do
+  #   !req.xhr? && req.format.html?
+  # end
+  root 'root#index'
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
