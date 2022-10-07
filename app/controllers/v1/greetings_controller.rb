53fc28c7ee6dad4dd1787ab7b33639bf96807a89
HSMathebula <56921581+HSMathebula@users.noreply.github.com>
2022-10-07 12:59:55 +0200

    Create an API endpoint that selects a random greeting from your table

---
+++ app/controllers/v1/greetings_controller.rb
@@ -0,0 +1,8 @@
+class V1::GreetingsController < ApplicationController
+    def index
+        count = Greeting.count
+        random_offset = rand(count)
+        @greeting = Greeting.offset(random_offset).first
+        render json: @greeting
+    end
+end
