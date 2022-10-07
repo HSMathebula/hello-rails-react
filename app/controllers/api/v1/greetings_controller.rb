cbe6e6d5f7e8fc21c8410a074414aa19ee0b9ed7
HSMathebula <56921581+HSMathebula@users.noreply.github.com>
2022-10-07 13:06:42 +0200

    Update index method in greetings controller

--- app/controllers/api/v1/greetings_controller.rb
+++ app/controllers/api/v1/greetings_controller.rb
@@ -1,8 +1,6 @@
 class Api::V1::GreetingsController < ApplicationController
     def index
-        count = Greeting.count
-        random_offset = rand(count)
-        @greeting = Greeting.offset(random_offset).first
+        @greeting = Greeting.order('RANDOM()').first
         render json: @greeting
     end
 end
